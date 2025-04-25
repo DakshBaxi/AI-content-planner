11d3404421367f0d8df30ddcad2d1abd89348a47
DakshBaxi <baxidaksh2004@gmail.com>
2025-04-25 23:21:59 +0530

    vercel node added

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 
 import express from "express"
 import cors from "cors"
-import serverless from "serverless-http"; // 👈 NEW
 import contentRoute from "./routes/contentRoute.js"
 import dotenv from 'dotenv';
 dotenv.config();
@@ -24,13 +23,12 @@ app.get('/',(req,res)=>{
 app.use('/api/plan-route',contentRoute)
 
 // Only listen locally if not running on Vercel
-if (process.env.NODE_ENV !== "production") {
-    const port = process.env.PORT || 5000;
+
+  
     app.listen(port, () => {
       console.log(`Server running locally on http://localhost:${port}`);
     });
-  }
+
   
-  // For Vercel serverless
-  export const handler = serverless(app);
+
   
